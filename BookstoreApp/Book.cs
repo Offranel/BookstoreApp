7e4e517e9ebb8641ed8d303ad57ad404bb868721
Offranel <francis.sengele@gmail.com>
2026-07-26 09:59:06 -0700

    Add Book class with Id, Title, and Price properties
    Created a Book class in the BookstoreApp namespace to represent books for sale. The class includes properties for Id (primary key), Title (required), and Price, along with XML documentation comments and necessary using directives.

---
+++ BookstoreApp/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Permissions;
+
+namespace BookstoreApp
+{
+    /// <summary>
+    /// Represents an indiduel book for sale
+    /// </summary>
+    
+    public class Book
+    {
+        /// <summary>
+        /// The primary key/unique identifier for the book 
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The title of the book
+        /// </summary>
+        public required string Title  { get; set; }
+
+        /// <summary>
+        /// Sales price of the book
+        /// </summary>
+        public double Price { get; set; }
+    }
+}
