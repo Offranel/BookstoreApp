98264491a673c4f18798b23d54e1ba003bf6e218
Offranel <francis.sengele@gmail.com>
2026-07-26 09:45:08 -0700

    Add BookStoreDb class for EF Core SQL Server support
    Added BookStoreDb class in BookstoreApp.Database, inheriting from DbContext. Configured Entity Framework Core to use a SQL Server connection. Included necessary using directives for required namespaces.

---
+++ BookstoreApp/Database/BookStoreDb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace BookstoreApp.Database;
+
+internal class BookStoreDb : DbContext 
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb);Database=BookstoreDb;Trusted_Connection=true; TrustServerCertificate=true");
+    }
+}
